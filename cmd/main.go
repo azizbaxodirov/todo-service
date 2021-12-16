abe34ab44a894ae4ca78fd63e53fffcda29e7bf7
azizbaxodirov <azizflaizy@gmail.com>
2021-12-16 20:15:38 +0500

    first task

---
+++ cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net"
+
+	"github.com/azizbaxodirov/todo-service/config"
+	pb "github.com/azizbaxodirov/todo-service/genproto"
+	"github.com/azizbaxodirov/todo-service/pkg/db"
+	"github.com/azizbaxodirov/todo-service/pkg/logger"
+	"github.com/azizbaxodirov/todo-service/service"
+	"github.com/azizbaxodirov/todo-service/storage"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	cfg := config.Load()
+
+	log := logger.New(cfg.LogLevel, "todo-service")
+	defer func(l logger.Logger) {
+		err := logger.Cleanup(l)
+		if err != nil {
+			log.Fatal("failed cleanup logger", logger.Error(err))
+		}
+	}(log)
+
+	log.Info("main: sqlxConfig",
+		logger.String("host", cfg.PostgresHost),
+		logger.Int("port", cfg.PostgresPort),
+		logger.String("database", cfg.PostgresDatabase))
+
+	connDB, err := db.ConnectToDB(cfg)
+	if err != nil {
+		log.Fatal("sqlx connection to postgres error", logger.Error(err))
+	}
+
+	pgStorage := storage.NewStoragePg(connDB)
+
+	todoService := service.NewTodoService(pgStorage, log)
+
+	lis, err := net.Listen("tcp", cfg.RPCPort)
+	if err != nil {
+		log.Fatal("Error while listening: %v", logger.Error(err))
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTodoServiceServer(s, todoService)
+
+	reflection.Register(s)
+
+	log.Info("main: server running",
+		logger.String("port", cfg.RPCPort))
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Error while listening: %v", logger.Error(err))
+	}
+}
